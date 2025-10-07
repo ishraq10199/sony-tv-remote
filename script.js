9c7a3c43d79b070937ddf5cf3850a27614dbdcab
Ishraq Hasan <ishraq10199@gmail.com>
2025-10-07 22:18:36 +0600

    #07 Oct 2025: Added more buttons

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 document.querySelector("#power").addEventListener("click", () => {
   console.log("Power button");
 });
+
+const numsEl = document.querySelector("#nums");
+const baseButtonClasses = [
+  "inline-flex",
+  "h-20",
+  "items-center",
+  "justify-center",
+  "px-6",
+  "font-medium",
+  "bg-neutral-950",
+  "text-neutral-50",
+];
+
+const numButtonClasses = [
+  ...baseButtonClasses,
+  "rounded-md",
+  "shadow-md",
+  "transition",
+  "active:scale-95",
+  "cursor-pointer",
+];
+
+for (let i = 0; i < 10; i++) {
+  const num = (i + 1) % 10;
+
+  if (num === 0) {
+    const padLeftOfZero = document.createElement("span");
+    padLeftOfZero.classList.add(...baseButtonClasses, "opacity-0");
+    numsEl.appendChild(padLeftOfZero);
+  }
+
+  const button = document.createElement("button");
+  button.classList.add(...numButtonClasses);
+
+  button.innerText = num;
+  numsEl.appendChild(button);
+}
